2446889565fe15f7b54969763c95140fd0789b4c
abhiyadav05 <abhshekyadav95065@gmail.com>
2026-03-17 17:16:27 +0530

    finished the controller of the chat server

--- backend/chat/src/controllers/chat.controller.ts
+++ backend/chat/src/controllers/chat.controller.ts
@@ -2,9 +2,10 @@ import axios from "axios";
 import { chatAuth, type AuthenticatedRequest } from "../middlewares/chatAuth.middleware.js";
 import { Chat } from "../models/chat.model.js";
 import { Messages } from "../models/messages.js";
+import { type Response } from "express";
 
 
-export const createNewChat = async (req : AuthenticatedRequest, res) => {
+export const createNewChat = async (req : AuthenticatedRequest, res:Response) => {
     try {
         const userId = req.user?._id;
         const {otherUserId} = req.body;
@@ -34,7 +35,7 @@ export const createNewChat = async (req : AuthenticatedRequest, res) => {
     }
 }
 
-export const getUserChats = async (req : AuthenticatedRequest, res) => {
+export const getUserChats = async (req : AuthenticatedRequest, res : Response) => {
     try {
         const userId = req.user?._id;
         if(!userId){
@@ -79,3 +80,56 @@ export const getUserChats = async (req : AuthenticatedRequest, res) => {
         res.status(500).json({message: 'Error fetching user chats'});
     }
 }
+
+export const sendMessage = async (req : AuthenticatedRequest, res : Response) => {
+    try {
+        const userId = req.user?._id;
+        const {chatId, text} = req.body;
+
+        // for image 
+        // const image = req.file?.path;
+        if(!userId){
+            return res.status(401).json({message: 'Unauthorized'});
+        }
+        if(!chatId || !text){
+            return res.status(400).json({message: 'Chat id and text are required'});
+        }
+
+        const chat = await Chat.findById(chatId);
+        if(!chat){
+            return res.status(404).json({message: 'Chat not found'});
+        }
+
+        const isUserInChat = chat.users.some((id) => id.toString() === userId.toString());
+        if(!isUserInChat){
+            return res.status(403).json({message: 'Forbidden'});
+        }
+        const otherUserId = chat.users.find((id) => id.toString() !== userId.toString());
+        if(!otherUserId){
+            return res.status(400).json({message: 'Invalid chat'});
+        }
+
+        //  socket setup 
+
+        let messageData : any = {
+            chatId,
+            sender: userId,
+            seen : false,
+            seenAt: undefined,
+        }
+
+        //  check for image 
+
+        messageData.text = text;
+
+        const message= new Messages(messageData);
+        const savedMessage = await message.save();
+        const latestMessage = text;
+        
+
+
+    }catch(error){
+        res.status(500).json({message: 'Error sending message'});
+    }
+}
+
--- backend/chat/src/routes/chat.route.ts
+++ backend/chat/src/routes/chat.route.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 import { chatAuth } from '../middlewares/chatAuth.middleware.js';
-import { createNewChat } from '../controllers/chat.controller.js';
+import { createNewChat, getUserChats } from '../controllers/chat.controller.js';
 
 const chatRouter = express.Router();
 
 
 chatRouter.post('/chat/new',chatAuth,createNewChat);
+chatRouter.get('/chat/all',chatAuth,getUserChats);
 
 export default chatRouter;
